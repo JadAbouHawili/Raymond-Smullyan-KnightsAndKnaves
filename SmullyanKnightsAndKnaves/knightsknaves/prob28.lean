20c5b4cd277cb34e2c746a9ebd59a172b05a53fa
Jad <jad-abou-hawili@hotmail.com>
2025-03-09 22:29:57 +0200

    some cleaning in prob28, more to come

--- SmullyanKnightsAndKnaves/knightsknaves/prob28.lean
+++ SmullyanKnightsAndKnaves/knightsknaves/prob28.lean
@@ -1,9 +1,33 @@
 import SmullyanKnightsAndKnaves.knightsknaves
+import SmullyanKnightsAndKnaves.dsl_knights_knaves
+import Lean
 -- prob28
-variable { K : Type}
-variable { A B : K}
-variable { x y : K}
+
+    #check Nat.lt_one_iff
+    #check Finset.card_eq_zero
+    #check Finset.subset_singleton_iff
+   #check Finset.subset_of_eq
+   #check Finset.empty_subset
+  #check Finset.ssubset_iff_subset_ne
+  #check Finset.subset_def
+  #check Finset.subset_iff
+theorem eq_of_or_not
+{A B w : Type}
+{p : Type → Prop}
+(AorB : w = A or w = B)
+(npA : ¬(p A))
+(pw : p w)
+
+: w = B := by 
+  rcases AorB with h|h
+  rw [h] at pw
+  contradiction
+
+  assumption
+
 example 
+  {K : Type}
+  {A B : K}
   {inst : DecidableEq K}
   {inst2 : Fintype K}
   (Knight : Finset K)
@@ -16,169 +40,115 @@ example
   (stA : A ∈ Knight ↔ Knave.card ≥ 1)
   (stAn : A ∈ Knave ↔ Knave.card < 1)
   : A ∈ Knight ∧ B ∈ Knave:= by
-
-  -- show that Knave.card<1 means Knave.card=0 which means Knave empty
-  have : Knave.card<1 → Knave = ∅ := by {
-    intro Klt1
-    rw [Nat.lt_one_iff] at Klt1
-    rw [Finset.card_eq_zero] at Klt1
-    assumption
-  }
+  #check Nat.lt_one_iff
+  rw [Nat.lt_one_iff] at stAn
+  rw [Finset.card_eq_zero] at stAn
 
 -- can form the statement A ∈ Knave → Knave=∅ 
 -- i.e A ∈ Knave → False
   have AnKnave : A ∈ Knave → False :=by
-    rw [stAn]
-    intro lt1
-    have knavemep := this lt1
+    intro AKnave
+    have := stAn.mp AKnave
+    rw [this] at AKnave
     contradiction
-  --_root_.trans stAn.mp this 
-  have AKnaveFalse : A ∈ Knave → False := by {
-    intro AKnave 
-    have Knaveemp:= AnKnave AKnave
-    --rw [Knaveemp] at AKnave
-    contradiction
-  }
-  -- when doing rw, if its implies then if the conclusionpattern matching then the hypothesis are new goals added
-  simp  at AKnaveFalse 
+  simp  at AnKnave
   -- get Knave.card ≥ 1
   -- get Knave ⊆ {A,B} , Knave ⊆ {B}, Knave.card ≤ 1
+  -- have not on both sides of stAn
+  #check not_iff_not
+  have notstAn:= not_iff_not.mpr stAn
+  have atLeastKnave := notstAn.mp AnKnave
+  simp at atLeastKnave
+
+  -- assuming B not knave gives contradiction
   #check Finset.subset_insert_iff_of_not_mem
-  have U : Finset.univ = {A,B}:= univ_iff_all2.mpr all
-   
-
-  -- either do like after the separator or this
-  have atleastoneKnave := stA.mp (by rw [inleft_notinrightIff (h'' A) h] ; assumption)
-   
-  ------------
-  -- this can be made simpler by a proper introduction to the idea of universe.
-  have : Knave ⊆ {A,B} := by 
-    rw [←U]
-    apply Finset.subset_univ
-
-  rw [Finset.subset_insert_iff_of_not_mem AKnaveFalse] at this 
-
-  have KleB:= Finset.card_le_card this
-  have oneB : ({B} : Finset K).card =1 := by 
-    rw [Finset.card_eq_one] 
-    use B
-  rw [oneB] at KleB 
-  have Kge := stA.mp (notinright_inleft (h'' A) AKnaveFalse)
-   
-  have oneKnave := Nat.le_antisymm KleB Kge
-  have knavenonemp : Knave ≠ ∅ := by 
-    intro emp
-    rw [emp] at oneKnave
+  #check Finset.subset_singleton_iff
+  have U : Finset.univ = ({A,B} : Finset K):= univ_iff_all2.mpr all
+  have ⟨w,winKnave⟩  :  ∃ w : K, w ∈ Knave := by 
+    by_contra h' 
+    push_neg at h'
+    #check Set.eq_empty_iff_forall_not_mem
+    have Knaveemp := Finset.eq_empty_of_forall_not_mem h'
     contradiction
-   
-  #check Finset.ssubset_iff_subset_ne
-  #check Finset.subset_def
-  #check Finset.subset_iff
+  #check eq_of_or_not
+  have AnKnave : ¬A ∈ Knave := by 
+    exact AnKnave 
 
-  -- make it into its own theorem
-  -- no need Finset.subset_singleton_iff is it
 
-  /-
-     for Knave ⊆ {A,B} ↔ Knave = ∅ ∨ Knave ={A,B}. this is not true
-  -/
-  have : Knave ⊆ {B} ↔ Knave =∅ ∨ Knave ={B} := by  
+  have BKnave : w=B := by 
+    rcases all w with h_1|h_2
+    rw [h_1] at winKnave 
+    contradiction
+
+    assumption 
+  rw [BKnave] at winKnave
+  constructor
+  rw [inleft_notinrightIff (h'' A) h]
+  assumption ; assumption
+
+#check all2_in_one_other_empty
+
+example
+  {K : Type}
+  {A B : K}
+  {Knight : Finset K}
+  {Knave : Finset K}
+  {inst : DecidableEq K}
+{h : Knight ∩ Knave = ∅ }
+{h1 : A ∈ Knight ∨ A ∈ Knave }
+{h2: B ∈ Knight ∨ B ∈ Knave }
+{stA : A ∈ Knight  ↔ ((A ∈ Knave) ∨ (B ∈ Knave)) }
+  : A ∈ Knight ∧ B ∈ Knave := by
+  {
+  -- book way, can't be further optimized
+    have AnKnave: A ∉ Knave := by 
+      intro AKnave
+      have AOrB : A ∈ Knave ∨ B ∈ Knave := by left ; exact AKnave 
+      have AKnight := stA.mpr AOrB
+      exact disjoint h AKnight AKnave
+
+    have AKnight := notinright_inleft h1 AnKnave 
     constructor
-    ·  
-      intro sub 
-      -- idk
-      exact Finset.subset_singleton_iff.mp this
-    · intro or
-      rcases or with h_1|h_2 
-      · rw [h_1]
-        apply Finset.empty_subset
+    · assumption
+    · have AknOrB := stA.mp AKnight
+      exact notleft_right AknOrB AnKnave 
+  }
 
-      #check Finset.subset_of_eq
-      exact Finset.subset_of_eq h_2
+--variable {K : Type}
 
--------------
-  have AnKnave : A ∉ Knave := by 
-    intro AKnave
-    have AKnave2 := AKnave
-    rw [inright_notinleftIff (h'' A) h]  at AKnave
-    have le1 := (Function.mt stA.mpr) AKnave
-    push_neg at le1
-    rw [Nat.lt_one_iff] at le1
-    rw [Finset.card_eq_zero] at le1
-    rw [le1] at AKnave2
-    contradiction
-  rw [notinright_inleftIff (h'' A) h] at AnKnave 
-  
-  have atleast := stA.mp AnKnave
-  have BnKnight : B ∉ Knight := by
-    intro BKnight
-    have Knaveemp := all2_in_one_other_empty h all AnKnave BKnight
-    rw [Knaveemp] at atleast
-    contradiction
-
-  rw [notinleft_inrightIff (h'' B) h] at BnKnight
-  exact And.intro AnKnave BnKnight
-
---variable (Knight : Finset K) (Knave : Finset K)
---
---#check XorToOr
---example
---  {inst : DecidableEq K}
---{h : Knight ∩ Knave = ∅ }
---{h1 : A ∈ Knight ∨ A ∈ Knave }
---{h2: B ∈ Knight ∨ B ∈ Knave }
---{stA : A ∈ Knight  ↔ ((A ∈ Knave) ∨ (B ∈ Knave)) }
---  : A ∈ Knight ∧ B ∈ Knave := by
---  {
---  -- book way, can't be further optimized
---    have AnKnave: A ∉ Knave := by 
---      intro AKnave
---      have AOrB : A ∈ Knave ∨ B ∈ Knave := by left ; exact AKnave 
---      have AKnight := stA.mpr AOrB
---      exact disjoint h AKnight AKnave
---
---    have AKnight := notinright_inleft h1 AnKnave 
---    constructor
---    · assumption
---    · have AknOrB := stA.mp AKnight
---      exact notleft_right AknOrB AnKnave 
---  }
---import Game.Metadata
-
-variable {K : Type}
-
---World "KnightsAndKnaves"
---Level 5
---
---Title ""
---
---Introduction "In this problem, there are only two people, A and B, each of 
---whom is either a knight or a knave. A makes the following 
---statement: 'At least one of us is a knave.' 
+--A: 'At least one of us is a knave.'
 --What are A and B? "
+def AtLeastOneIsKnave (A B : Islander) : Prop := (A.isKnave) ∨ (B.isKnave)
+example 
+{A B : Islander}
+{stA : A said (A.isKnave or B.isKnave)}
+: A.isKnight and B.isKnave := by 
+   
+  sorry
 
-open Lean Parser Elab Tactic
-elab "show_goal" t:tactic : tactic => do
-  logInfoAt t m!"⊢ {(← Elab.Tactic.getMainTarget)}"
-  evalTactic t
-
-def getTacs (t1 : Syntax) : Array Syntax :=
-  match t1 with
-    | .node _ ``tacticSeq #[.node _ ``tacticSeq1Indented #[.node _ `null args]] =>
-      args.filter (! ·.isOfKind `null)
-    | _ => #[t1]
-
-elab "show_goals1 " tacs:tacticSeq : tactic => do
-  let tacs := getTacs tacs
-  for t in tacs do
-    evalTactic (← `(tactic| show_goal $(⟨t⟩)))
-
-elab "show_goals " tacs:tacticSeq : tactic => do
-  let tacs := getTacs tacs
-  let _ ← tacs.mapM fun t => do
-    match t with
-      | `(tactic| · $ts) => evalTactic (← `(tactic| · show_goals1 $(⟨ts.raw⟩)))
-      | _ => evalTactic (← `(tactic| show_goals1 $(⟨t⟩)))
-
+--open Lean Parser Elab Tactic
+--elab "show_goal" t:tactic : tactic => do
+--  logInfoAt t m!"⊢ {(← Elab.Tactic.getMainTarget)}"
+--  evalTactic t
+--
+--def getTacs (t1 : Syntax) : Array Syntax :=
+--  match t1 with
+--    | .node _ ``tacticSeq #[.node _ ``tacticSeq1Indented #[.node _ `null args]] =>
+--      args.filter (! ·.isOfKind `null)
+--    | _ => #[t1]
+--
+--elab "show_goals1 " tacs:tacticSeq : tactic => do
+--  let tacs := getTacs tacs
+--  for t in tacs do
+--    evalTactic (← `(tactic| show_goal $(⟨t⟩)))
+--
+--elab "show_goals " tacs:tacticSeq : tactic => do
+--  let tacs := getTacs tacs
+--  let _ ← tacs.mapM fun t => do
+--    match t with
+--      | `(tactic| · $ts) => evalTactic (← `(tactic| · show_goals1 $(⟨ts.raw⟩)))
+--      | _ => evalTactic (← `(tactic| show_goals1 $(⟨t⟩)))
+--
 --(uni : Knight ∪ Knave) 
 -- theres x and y, x says at least one of us is a knave
   -- diff formalization to stx
@@ -188,8 +158,9 @@ elab "show_goals " tacs:tacticSeq : tactic => do
 
 --Raymond Smullyan, what is the name of this book, problem 28
 example 
+  { K : Type}
+  {x y : K}
   {inst : DecidableEq K}
-  { x y : K}
   (Knight : Finset K ) (Knave : Finset K) 
   --(uni : Knight ∪ Knave) 
   (h : Knight ∩ Knave = ∅ )
@@ -323,6 +294,7 @@ example
 -- rewriting, making it neat
 theorem organized 
   --sets
+  {K : Type}
   { x y : K}
   (Knight : Set K ) (Knave : Set K)
   (h : Knight ∩ Knave = ∅ )
@@ -378,6 +350,8 @@ cases h1 with
 
 -- prob28
 example 
+  {K : Type}
+  {A B : K}
   {inst : DecidableEq K}
   {inst2 : Fintype K}
   (Knight : Finset K)
@@ -487,11 +461,11 @@ example
   exact And.intro AnKnave BnKnight
 
 
-
-import knightsknaves
 -----------------
 -- prob28
 example 
+  {K : Type}
+  {A B : K}
   {inst : DecidableEq K}
   {inst2 : Fintype K}
   (Knight : Finset K)

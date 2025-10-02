319a725dc2935c25a983af7c7eadf5f2778ae008
Jad <jad-abou-hawili@hotmail.com>
2025-10-02 10:50:21 +0300

    lake build target , need to add imports to all other files

--- SmullyanKnightsAndKnaves.lean
+++ SmullyanKnightsAndKnaves.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `SmullyanKnightsAndKnaves` library.
 -- Import modules here that should be built as part of the library.
-import SmullyanKnightsAndKnaves.knightsknaves
+import SmullyanKnightsAndKnaves.Build
